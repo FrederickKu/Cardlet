597e75461218355f546882fca6f4491f20028976
FrederickKu <timothyfrederick.ku@gmail.com>
2019-09-11 15:54:44 +0800

    fixed modal in backend renders

--- src/server/views/frontend/userchoice.jsx
+++ src/server/views/frontend/userchoice.jsx
@@ -1,5 +1,6 @@
 var React = require("react");
 import {isMobile} from "react-device-detect";
+import Modal from './modal'
 
 class Choice extends React.Component {
 
@@ -51,10 +52,11 @@ class Choice extends React.Component {
                         <p><a href={"/wallet"}><i className={'bx bx-id-card'}></i></a></p>
                     </div>
                     <div className={`footerPerson`}>
-                        <p><i className={'bx bx-user'}></i></p>
+                        <p><i className={'bx bx-user'} data-toggle={"modal"} data-target={"#userModal"}></i></p>
                     </div>
                 </div>
             </div>
+            <Modal />
             <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
