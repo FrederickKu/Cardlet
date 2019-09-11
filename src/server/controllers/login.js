d665307ad6741e702dad9c45195e7b800d1211ac
FrederickKu <timothyfrederick.ku@gmail.com>
2019-09-11 16:38:03 +0800

    fix login error

--- src/server/controllers/login.js
+++ src/server/controllers/login.js
@@ -12,66 +12,6 @@ cloudinary.config({
 
 module.exports = (db) => {
 
-  let performOCR = function (imagePath, returnFunction) {
-    console.log("ABBYY Cloud OCR SDKfor Node.js");
-
-    var ocrsdk = ocrsdkModule.create(appId, password, serviceUrl);
-
-    var settings = new ocrsdkModule.ProcessingSettings();
-    // Set your own recognition language and output format here
-    settings.language = "English"; // Can be comma-separated list, e.g. "German,French".
-    settings.exportFormat = "xml"; // All possible values are listed in 'exportFormat' parameter description
-                                    // at https://ocrsdk.com/documentation/apireference/processImage/
-
-
-        function downloadCompleted(error, results) {
-            if (error) {
-                console.log("Error1: " + error.message);
-                return;
-            }
-            console.log("Done.");
-            returnFunction(results);
-        }
-
-        function processingCompleted(error, taskData) {
-            if (error) {
-                console.log("Error2: " + error.message);
-                return;
-            }
-
-            if (taskData.status != 'Completed') {
-                console.log("Error processing the task.");
-                if (taskData.error) {
-                    console.log("Message: " + taskData.error);
-                }
-                return;
-            }
-
-            console.log("Processing completed.");
-            console.log("Downloading results");
-
-            ocrsdk.downloadResult(taskData.resultUrl.toString(), downloadCompleted);
-        }
-
-        function uploadCompleted(error, taskData) {
-            if (error) {
-                console.log("Error3: " + error.message);
-                return;
-            }
-
-            console.log("Upload completed.");
-            console.log("Task id = " + taskData.id + ", status is " + taskData.status);
-            if (!ocrsdk.isTaskActive(taskData)) {
-                console.log("Unexpected task status " + taskData.status);
-                return;
-            }
-
-            ocrsdk.waitForCompletion(taskData.id, processingCompleted);
-        }
-
-        console.log("Uploading image..");
-        ocrsdk.processImage(imagePath, settings, uploadCompleted);
-  }
 
   let displayLogin = async function (request, response) {
     response.render('login/login');
