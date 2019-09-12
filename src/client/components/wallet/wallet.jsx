fe172eb734dc8e6b33bf47a0b991a6326361f645
FrederickKu <timothyfrederick.ku@gmail.com>
2019-09-12 09:20:05 +0800

    working search

--- src/client/components/wallet/wallet.jsx
+++ src/client/components/wallet/wallet.jsx
@@ -9,17 +9,19 @@ class Wallet extends React.Component {
         this.state = {
             userWallet: [],
             isLoaded: false,
-            error: ''
+            error: '',
+            display: []
         }
 
         this.deleteCard=this.deleteCard.bind(this);
+        this.search=this.search.bind(this);
     }
 
     componentDidMount() {
         fetch("/getUserDetails")
         .then(response => response.json())
         .then((result) => {
-            this.setState({userWallet: result.userWallet, isLoaded:true})
+            this.setState({userWallet: result.userWallet,  display: result.userWallet, isLoaded:true})
         },
         (error) =>{
             this.setState({error: error})
@@ -44,16 +46,33 @@ class Wallet extends React.Component {
         })
         .then(response => response.json())
         .then((result) => {
-            this.setState({userWallet:result.userWallet, isLoaded:true })
+            this.setState({userWallet:result.userWallet, display:result.userWallet, isLoaded:true })
         },
         (error) =>{
                 this.setState({error: error})
         })
     }
 
+    search(event){
+        let userWallet = this.state.userWallet;
+        let searchWord = event.target.value;
+        let searchResult = userWallet.filter ( (card) => (
+            card.name.includes(searchWord) ||
+            card.title.includes(searchWord) ||
+            card.phone.includes(searchWord) ||
+            card.mobile.includes(searchWord) ||
+            card.email.includes(searchWord) ||
+            card.website.includes(searchWord) ||
+            card.address.includes(searchWord) ||
+            card.company.includes(searchWord)
+        ));
+
+        this.setState({display:searchResult})
+    }
+
     render(){
         if (this.state.isLoaded) {
-            let allCards = this.state.userWallet.map( (card,index) => {
+            let allCards = this.state.display.map( (card,index) => {
                 return (
                     <div className={`${style.individualCard}`} key={index}>
                         <div className = {`${style.cardImage}`} style={{backgroundImage:`url(${card.namecard_image})`}}></div>
@@ -76,7 +95,7 @@ class Wallet extends React.Component {
                         <p> Business Card Wallet </p>
                         <form className={"form-inline"}>
                             <i className={'bx bx-search'}></i>
-                            <input  type={"text"} placeholder={"Search"} aria-label={"Search"} />
+                            <input  type={"text"} placeholder={"Search"} aria-label={"Search"} onChange={this.search} />
                         </form>
                         <div className={`${style.cardContainer}`}>
                             {allCards}
